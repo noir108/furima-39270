73c114edfa25952db6c6b1402cd52f994b7d1e4e
noir108 <tymbsd10@gmail.com>
2023-05-08 20:34:43 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
 
+
+  with_options presence: true do
+    validates :name, :content, :price, :image
+  end
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+  validates :category_id, :status_id, :shipping_fee_id, :area_id, :lead_time_id
+  end
+
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
