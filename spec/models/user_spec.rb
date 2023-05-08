e184fd07a0f24b89a30041f68e9de44009b3aa6c
noir108 <tymbsd10@gmail.com>
2023-05-08 14:22:51 +0900

    テストコードの準備

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nicknameとemail、passwordとpassword_confirmation、名前のseiとmei、名前のsei_kanaとmei_kanaとbirthdayが存在すれば登録できる" do
+
+      end
+    end
+    context '新規登録できない場合' do
+      it "nicknameが空では登録できない" do
+
+      end
+      it "emailが空では登録できない" do
+
+      end
+      it '重複したemailが存在する場合は登録できない' do
+
+      end
+      it 'emailは@を含まないと登録できない' do
+
+      end
+      it "passwordが空では登録できない" do
+
+      end
+      it 'passwordが5文字以下では登録できない' do
+
+      end
+      it 'passwordが129文字以上では登録できない' do
+
+      end
+      it 'passwordが半角英数字混合じゃないと登録できない' do
+
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+
+      end
+      it "seiが空では登録できない" do
+
+      end
+      it "meiが空では登録できない" do
+
+      end
+      it "seiが全角（漢字・ひらがな・カタカナ）以外だと登録できない" do
+
+      end
+      it "meiが全角（漢字・ひらがな・カタカナ）以外だと登録できない" do
+
+      end
+      it "sei_kanaが空では登録できない" do
+
+      end
+      it "mei_kanaが空では登録できない" do
+
+      end
+      it "sei_kanaが全角（カタカナ）以外だと登録できない" do
+
+      end
+      it "mei_kanaが全角（カタカナ）以外だと登録できない" do
+
+      end
+      it "birthdayが空では登録できない" do
+
+      end
+    end
+  end
 end
