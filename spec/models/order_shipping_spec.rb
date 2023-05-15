f990a52aa641d865670b9e699592d7cb8a2931d4
noir108 <tymbsd10@gmail.com>
2023-05-15 20:36:31 +0900

    テストコード追加

--- spec/models/order_shipping_spec.rb
+++ spec/models/order_shipping_spec.rb
@@ -84,6 +84,11 @@ RSpec.describe OrderShipping, type: :model do
         @order_shipping.valid?
         expect(@order_shipping.errors.full_messages).to include("Item can't be blank")
       end
+      it "tokenが空では登録できないこと" do
+        @order_shipping.token = ''
+        @order_shipping.valid?
+        expect(@order_shipping.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
